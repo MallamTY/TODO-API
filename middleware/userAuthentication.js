f658a734ae16cb2bd0f9be1b57754cd8b48c8ad3
Temitayo Sosanya <sosanyatemitayonurudeen@gmail.com>
2022-08-21 21:54:54 +0100

    Adding authorization to TODO-API

--- middleware/userAuthentication.js
+++ middleware/userAuthentication.js
@@ -10,7 +10,7 @@ const userAuthentication = async(req, res, next) => {
     if(!authorization) {
         return res.status(401).json('Authorization token required !!!!!!!!!!')
     }
-    
+
     const token = authorization.split(' ')[1]
     
     if(!token) {
@@ -26,7 +26,7 @@ const userAuthentication = async(req, res, next) => {
     } catch (error) {
         res.status(500).json({error: error.message})
     }
-
+//kbsvdsdvsjdcsdkskld
 }
 
 module.exports = userAuthentication
